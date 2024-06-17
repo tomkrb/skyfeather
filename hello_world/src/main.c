d450a6c78400ac1b350eedff9a2d01dd5c573d83
Tom Kr. Bardal <tom@trada.no>
2024-06-17 20:10:02 +0200

    working button press and two line text

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -4,10 +4,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/* Screen */
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/display/cfb.h>
 #include <stdio.h>
+
+/* Buttons */
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/devicetree/gpio.h>
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define BTN_A_NODE	DT_NODELABEL(buttona)
+#define BTN_B_NODE	DT_NODELABEL(buttonb)
+#define BTN_C_NODE	DT_NODELABEL(buttonc)
+#if !DT_NODE_HAS_STATUS(BTN_A_NODE, okay) || !DT_NODE_HAS_STATUS(BTN_B_NODE, okay)  || !DT_NODE_HAS_STATUS(BTN_C_NODE, okay) 
+#error "Unsupported board: button_a, b or c devicetree node(s) is/are not defined"
+#endif
+
+static const struct gpio_dt_spec buttona = GPIO_DT_SPEC_GET_OR(BTN_A_NODE, gpios, {0});
+static const struct gpio_dt_spec buttonb = GPIO_DT_SPEC_GET_OR(BTN_B_NODE, gpios, {0});
+static const struct gpio_dt_spec buttonc = GPIO_DT_SPEC_GET_OR(BTN_C_NODE, gpios, {0});
+
+static struct gpio_callback button_cb_data;
+
+void configure_button(const struct gpio_dt_spec *button)
+{
+	if (!device_is_ready(button->port)) {
+		printk("Button device not ready: %s\n", button->port->name);
+		return;
+	}
+
+	int ret = gpio_pin_configure_dt(button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n", ret, button->port->name, button->pin);
+	}
+
+	printk("Success.  Configured %s pin %d\n", button->port->name, button->pin);
+}
 
 int main(void)
 {
@@ -18,6 +55,10 @@ int main(void)
 	uint8_t ppt;
 	uint8_t font_width;
 	uint8_t font_height;
+
+	configure_button(&buttona);
+	configure_button(&buttonb);
+	configure_button(&buttonc);
 
 	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(dev)) {
@@ -60,6 +101,8 @@ int main(void)
 		printf("font width %d, font height %d\n",
 		       font_width, font_height);
 	}
+	cfb_framebuffer_set_font(dev, 0);
+
 
 	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
 	       x_res,
@@ -73,20 +116,31 @@ int main(void)
 	cfb_set_kerning(dev, 3);
 
 	while (1) {
-		for (int i = 0; i < MIN(x_res, y_res); i++) {
-			cfb_framebuffer_clear(dev, true);
-			if (cfb_print(dev,
-				      "0123456789mMgj!\"§$%&/()=",
-				      i, i)) {
-				printf("Failed to print a string\n");
-				continue;
-			}
-			printf("i = %d\n", i);
-			cfb_framebuffer_finalize(dev);
-#if defined(CONFIG_ARCH_POSIX)
-			k_sleep(K_MSEC(20));
-#endif
+		int vala = gpio_pin_get_dt(&buttona);
+		int valb = gpio_pin_get_dt(&buttonb);
+		int valc = gpio_pin_get_dt(&buttonc);
+
+		printf("read keys: %d %d %d\n", vala, valb, valc);
+
+		cfb_framebuffer_clear(dev, true);
+		char *str;
+		str = "HELLO";
+		if( vala) 
+			str = "AAAAA";
+		if( valb) 
+			str = "BBBBB";
+		if( valc) 
+			str = "CCCCC";
+		
+		if (cfb_print(dev, str, 0, 0)) {
+			printf("Failed to print a string\n");
+			continue;
 		}
+		// printf("i = %d\n", i);
+		cfb_framebuffer_finalize(dev);
+#if defined(CONFIG_ARCH_POSIX)
+		k_sleep(K_MSEC(20));
+#endif
 	}
 	return 0;
 }
