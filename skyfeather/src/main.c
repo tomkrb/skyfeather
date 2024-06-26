34ce3c4344aa42d754aedf8e394fc4c53414b9c5
Tom Kr. Bardal <tom@trada.no>
2024-06-26 20:57:08 +0200

    building with seesaw shield included

--- skyfeather/src/main.c
+++ skyfeather/src/main.c
@@ -32,7 +32,7 @@ static const struct gpio_dt_spec buttona = GPIO_DT_SPEC_GET_OR(BTN_A_NODE, gpios
 static const struct gpio_dt_spec buttonb = GPIO_DT_SPEC_GET_OR(BTN_B_NODE, gpios, {0});
 static const struct gpio_dt_spec buttonc = GPIO_DT_SPEC_GET_OR(BTN_C_NODE, gpios, {0});
 
-const struct device *const lsm6ds0 = DEVICE_DT_GET_ONE(st_lsm6ds0);
+const struct device *const lsm6dso = DEVICE_DT_GET_ONE(st_lsm6dso);
 
 void configure_button(const struct gpio_dt_spec *button)
 {
@@ -49,7 +49,7 @@ void configure_button(const struct gpio_dt_spec *button)
 	printk("Success.  Configured %s pin %d\n", button->port->name, button->pin);
 }
 
-int init_screen(const struct device *dev)
+int init_screen(const struct device *display)
 {
 	uint16_t x_res;
 	uint16_t y_res;
@@ -58,8 +58,8 @@ int init_screen(const struct device *dev)
 	uint8_t font_width;
 	uint8_t font_height;
 
-	if (display_set_pixel_format(dev, PIXEL_FORMAT_MONO10) != 0) {
-		if (display_set_pixel_format(dev, PIXEL_FORMAT_MONO01) != 0) {
+	if (display_set_pixel_format(display, PIXEL_FORMAT_MONO10) != 0) {
+		if (display_set_pixel_format(display, PIXEL_FORMAT_MONO01) != 0) {
 			printf("Failed to set required pixel format");
 			return 0;
 		} else {
@@ -69,32 +69,32 @@ int init_screen(const struct device *dev)
 		printf("display_set....(MONO10)\n");
 	}
 
-	printf("Initialized %s\n", dev->name);
+	printf("Initialized %s\n", display->name);
 
-	if (cfb_framebuffer_init(dev)) {
+	if (cfb_framebuffer_init(display)) {
 		printf("Framebuffer initialization failed!\n");
 		return 0;
 	}
 
 
-	cfb_framebuffer_clear(dev, true);
+	cfb_framebuffer_clear(display, true);
 
-	display_blanking_off(dev);
+	display_blanking_off(display);
 
-	x_res = cfb_get_display_parameter(dev, CFB_DISPLAY_WIDTH);
-	y_res = cfb_get_display_parameter(dev, CFB_DISPLAY_HEIGH);
-	rows = cfb_get_display_parameter(dev, CFB_DISPLAY_ROWS);
-	ppt = cfb_get_display_parameter(dev, CFB_DISPLAY_PPT);
+	x_res = cfb_get_display_parameter(display, CFB_DISPLAY_WIDTH);
+	y_res = cfb_get_display_parameter(display, CFB_DISPLAY_HEIGH);
+	rows = cfb_get_display_parameter(display, CFB_DISPLAY_ROWS);
+	ppt = cfb_get_display_parameter(display, CFB_DISPLAY_PPT);
 
 	for (int idx = 0; idx < 42; idx++) {
-		if (cfb_get_font_size(dev, idx, &font_width, &font_height)) {
+		if (cfb_get_font_size(display, idx, &font_width, &font_height)) {
 			break;
 		}
-		cfb_framebuffer_set_font(dev, idx);
+		cfb_framebuffer_set_font(display, idx);
 		printf("font width %d, font height %d\n",
 		       font_width, font_height);
 	}
-	cfb_framebuffer_set_font(dev, 0);
+	cfb_framebuffer_set_font(display, 0);
 
 
 	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
@@ -102,11 +102,11 @@ int init_screen(const struct device *dev)
 	       y_res,
 	       ppt,
 	       rows,
-	       cfb_get_display_parameter(dev, CFB_DISPLAY_COLS));
+	       cfb_get_display_parameter(display, CFB_DISPLAY_COLS));
 
-	// cfb_framebuffer_invert(dev);
+	// cfb_framebuffer_invert(display);
 
-	// cfb_set_kerning(dev, 3);
+	// cfb_set_kerning(display, 3);
 
 	return 0;
 }
@@ -114,47 +114,55 @@ int init_screen(const struct device *dev)
 void joy_feather(void)
 {}
 
+void get_accelerometer(double *x, double *y, double *z)
+{
+		struct sensor_value accel_xyz[3];
+
+		if (sensor_sample_fetch(lsm6dso) < 0) {
+			printf("LSM6DSO Sensor sample update error\n");
+		}
+		sensor_channel_get(lsm6dso, SENSOR_CHAN_ACCEL_XYZ, accel_xyz);
+
+		printf(
+		   "LSM6DSO: Acceleration (m.s-2): x: %.1f, y: %.1f, z: %.1f\n",
+		   sensor_value_to_double(&accel_xyz[0]),
+		   sensor_value_to_double(&accel_xyz[1]),
+		   sensor_value_to_double(&accel_xyz[2]));
+
+		*x = sensor_value_to_double(&accel_xyz[0]);
+		*y = sensor_value_to_double(&accel_xyz[1]);
+		*z = sensor_value_to_double(&accel_xyz[2]);
+}	
+
 int main(void)
 {
-	const struct device *dev;
-	struct sensor_value accel_xyz[3];
+	const struct device *display;
 	char output_str[64];
 
 	configure_button(&buttona);
 	configure_button(&buttonb);
 	configure_button(&buttonc);
 
-	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
-	if (!device_is_ready(dev)) {
-		printf("Device %s not ready\n", dev->name);
+	display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display)) {
+		printf("Device %s not ready\n", display->name);
 		return 0;
 	}
 
-	init_screen(dev);
+	init_screen(display);
 
-	if (!device_is_ready(lsm6ds0)) {
-		printk("%s: device not ready.\n", lsm6ds0->name);
+	if (!device_is_ready(lsm6dso)) {
+		printk("%s: device not ready.\n", lsm6dso->name);
 		// return 0;
 	}
 
+
 	while (1) {
-		if (sensor_sample_fetch(lsm6ds0) < 0) {
-			printf("LSM6DS0 Sensor sample update error\n");
-			return 0;
-		}
-		sensor_channel_get(lsm6ds0, SENSOR_CHAN_ACCEL_XYZ, accel_xyz);
-
-		printf(
-		   "LSM6DS0: Acceleration (m.s-2): x: %.1f, y: %.1f, z: %.1f\n",
-		   sensor_value_to_double(&accel_xyz[0]),
-		   sensor_value_to_double(&accel_xyz[1]),
-		   sensor_value_to_double(&accel_xyz[2]));
-
+		double acc_x, acc_y, acc_z;
+		get_accelerometer(&acc_x, &acc_y, &acc_z);
 		snprintf(output_str, 63,
 		   "%4.1f %4.1f %4.1f        ",
-		   sensor_value_to_double(&accel_xyz[0]),
-		   sensor_value_to_double(&accel_xyz[1]),
-		   sensor_value_to_double(&accel_xyz[2]));
+		   acc_x, acc_y, acc_z);
 
 		int vala = gpio_pin_get_dt(&buttona);
 		int valb = gpio_pin_get_dt(&buttonb);
@@ -169,18 +177,53 @@ int main(void)
 		   valb ? 'B' : 'b',
 		   valc ? 'C' : 'c');
 		   
-		// cfb_framebuffer_clear(dev, true);
-		if (cfb_print(dev, key_str, 0, 0)) {
+		// cfb_framebuffer_clear(display, true);
+		if (cfb_print(display, key_str, 0, 0)) {
 			printf("Failed to print a string\n");
 			continue;
 		}
-		if (cfb_print(dev, output_str, 0, 16)) {
+		if (cfb_print(display, output_str, 0, 16)) {
 			printf("Failed to print a string\n");
 			continue;
 		}
 		
 		// printf("i = %d\n", i);
-		cfb_framebuffer_finalize(dev);
+		cfb_framebuffer_finalize(display);
+		k_sleep(K_MSEC(50));
+	}
+	
+	while (1) {
+		double acc_x, acc_y, acc_z;
+		get_accelerometer(&acc_x, &acc_y, &acc_z);
+		snprintf(output_str, 63,
+		   "%4.1f %4.1f %4.1f        ",
+		   acc_x, acc_y, acc_z);
+
+		int vala = gpio_pin_get_dt(&buttona);
+		int valb = gpio_pin_get_dt(&buttonb);
+		int valc = gpio_pin_get_dt(&buttonc);
+
+		printf("read keys: %d %d %d\n", vala, valb, valc);
+
+		char key_str[64];
+		snprintf(key_str, 63,
+		   "%c %c %c 	  ",
+		   vala ? 'A' : 'a',
+		   valb ? 'B' : 'b',
+		   valc ? 'C' : 'c');
+		   
+		// cfb_framebuffer_clear(display, true);
+		if (cfb_print(display, key_str, 0, 0)) {
+			printf("Failed to print a string\n");
+			continue;
+		}
+		if (cfb_print(display, output_str, 0, 16)) {
+			printf("Failed to print a string\n");
+			continue;
+		}
+		
+		// printf("i = %d\n", i);
+		cfb_framebuffer_finalize(display);
 		k_sleep(K_MSEC(50));
 	}
 	return 0;
